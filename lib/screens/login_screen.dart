9006c1ff8a940a48fd7afe34d39f7daf2be985d4
Aditya <adityabhatt78910@gmail.com>
2023-08-11 00:51:27 +0530

    login page ui

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sociyo/utils/colors.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -13,49 +14,136 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: quartz,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Padding(
-                padding: EdgeInsets.only(right: 0, top: 60),
-                child: Image(
-                  image: AssetImage(
-                      '/Users/adityabhatt/Documents/sociyo_app/assets/images/S(thin).png'),
-                )),
-            Padding(
-              padding: const EdgeInsets.all(32),
-              child: Container(
-                child: Column(
-                  children: [
-                    const Text(
-                      "Login",
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                    ),
-                    HeightBox(35),
-                    TextFormField(
-                      style: TextStyle(height: .7),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5)),
-                        labelText: "Email",
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Padding(
+                  padding: EdgeInsets.only(right: 0, top: 60),
+                  child: Image(
+                    image: AssetImage(
+                        '/Users/adityabhatt/Documents/sociyo_app/assets/images/S(thin).png'),
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(25),
+                          onTap: () {},
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(bottom: 8, top: 8),
+                                child: Image(
+                                  image: AssetImage(
+                                      "/Users/adityabhatt/Documents/sociyo_app/assets/images/google-icon-2048x2048-czn3g8x8.png"),
+                                  width: 50,
+                                  height: 50,
+                                ),
+                              ),
+                              Text(
+                                "Contiue with google",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                    HeightBox(5),
-                    TextFormField(
-                      style: TextStyle(height: .7),
-                      decoration: InputDecoration(
+                      const HeightBox(15),
+                      const Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.black,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Text(
+                              "or",
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.black,
+                            ),
+                          )
+                        ],
+                      ),
+                      const HeightBox(8),
+                      TextFormField(
+                        style: const TextStyle(height: .7),
+                        decoration: InputDecoration(
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(5)),
-                          labelText: "Password"),
-                    )
-                  ],
+                          labelText: "Email",
+                        ),
+                      ),
+                      const HeightBox(5),
+                      TextFormField(
+                        style: const TextStyle(height: .7),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5)),
+                            labelText: "Password"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 220),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text("Forgot Password?"),
+                        ),
+                      ),
+                      const HeightBox(35),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          alignment: AlignmentDirectional.center,
+                          width: 90,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: violet,
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don't have account yet?"),
+                          TextButton(
+                              onPressed: () => {}, child: const Text("Sign up"))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,3 +4,4 @@ const quartz = Color(0xFFFFFDE0);
 const purple = Color(0xFFA16AE8);
 const iris = Color(0xFF4120A9);
 const periwinkle = Color(0xFFA1A9FE);
+const violet = Color(0xFF8C52FF);
